a41712f4efd8e16f28b6d06c78ae7a49ddb0e657
Sebastian M. Schramm <schrammsm@gmail.com>
2026-02-05 00:08:16 +0000

    update agent

--- an_agent.py
+++ an_agent.py
@@ -1,4 +1,7 @@
+from typing import List, Callable
+
 from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 
@@ -9,21 +12,42 @@ def add_numbers(number_a: float, number_b: float) -> float:
     """
     return number_a + number_b
 
-my_tools = [add_numbers]
+
+@tool
+def clarification_rephraser(last_user_message: str, last_ai_response: str) -> str:
+    """Use this tool whne a user asked for a clarification of the last AI message.
+    """
+    clarification_prompt = [
+        SystemMessage("You are a communication and rephrasing expert. Rephrase the last AI message to clarify the users confusion."),
+        HumanMessage(f"AI said: {last_ai_response}. User asked: {last_user_message}. Provide a clearer rephrased AI message.")
+        ]
+    llm = get_llm()
+    response = llm.invoke(clarification_prompt)
+    print(f"Clarifiaction RESPONSE: {response.content}")
+    return response.content
 
 
-llm_name = "gemini-2.5-flash"
-
-llm = ChatGoogleGenerativeAI(model=llm_name, temperature=0.0)
-llm = llm.bind_tools(my_tools)
+my_tools = [add_numbers, clarification_rephraser]
 
 
+def get_llm(llm_name: str = "gemini-2.5-flash", tools: List[Callable] = None) -> ChatGoogleGenerativeAI:
+    llm = ChatGoogleGenerativeAI(model=llm_name, temperature=0.0)
+    if tools:
+        llm = llm.bind_tools(tools)
+    return llm
 
-prompt = "What is the sum 2 and 5?"
 
-response = llm.invoke(prompt)
+if __name__ == "__main__":
+    llm = get_llm(tools=my_tools)
+    
+    #prompt = "What is the sum 2 and 5?"
+    prompt = [AIMessage("What are the main metrics you should consider for scaling your load based scaling on EKS?"), HumanMessage("I don't understand what EKS means?")]
 
-tc = response.tool_calls[0]
-print(add_numbers.invoke(tc))
-print(response)
+    response = llm.invoke(prompt)
+    print(response)
+
+    tc = response.tool_calls[0]
+    print(clarification_rephraser.invoke(tc))
+    # print(add_numbers.invoke(tc))
+    
 
